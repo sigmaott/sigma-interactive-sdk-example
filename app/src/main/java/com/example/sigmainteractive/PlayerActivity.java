9b7c795a30db269fe59dc3f265d0bb026a49bdc7
haipn <haipn32@wru.vn>
2022-03-29 16:16:28 +0700

    update sdk

--- app/src/main/java/com/example/sigmainteractive/PlayerActivity.java
+++ app/src/main/java/com/example/sigmainteractive/PlayerActivity.java
@@ -47,6 +47,7 @@ public class PlayerActivity extends Activity {
     private static final String sourcePlay = "https://dev-livestream.gviet.vn/manifest/VTV2-PACKAGE/master.m3u8";
     ExoPlayer player;
     Boolean isPlaying;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,7 +68,7 @@ public class PlayerActivity extends Activity {
                             String value = ((TextInformationFrame) entry).value;
                             Log.d("DesString=>Instance", des);
                             if (des.toUpperCase().equals("TXXX")) {
-                                if(SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).getInteractiveView() != null) {
+                                if (SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).getInteractiveView() != null) {
                                     SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).sendID3InstantInteractive(value);
                                 }
                             }
@@ -88,7 +89,7 @@ public class PlayerActivity extends Activity {
                             String value = ((TextInformationFrame) entry).value;
                             Log.d("DesString=>", des);
                             if (des.toUpperCase().equals("TXXX")) {
-                                if(SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).getInteractiveView() != null) {
+                                if (SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).getInteractiveView() != null) {
                                     SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).sendID3Interactive(value);
                                 }
                             }
@@ -112,7 +113,8 @@ public class PlayerActivity extends Activity {
             SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).setLayoutInteractiveView(0, 0, containerView.getLayoutParams().width, containerView.getLayoutParams().height, containerView.getLayoutParams().width, containerView.getLayoutParams().height, 0, 0);
         }
     }
-    private void setupPlayer(){
+
+    private void setupPlayer() {
         Uri videoUri = Uri.parse(sourcePlay);
         MediaItem mediaItem = MediaItem.fromUri(videoUri);
         // Set the media item to be played.
@@ -129,46 +131,73 @@ public class PlayerActivity extends Activity {
         this.openInteractiveView(0, 0, width, height, width, height, 0, 0, null);
     }
 
-    private void openInteractiveView(int x, int y, int width, int height, int widthPlayer, int heightPlayer, int xPlayer, int yPlayer, Bundle userData) {
+    private void openInteractiveView(int xInteractiveView, int yInteractiveView, int widthInteractiveView, int heightInteractiveView, int widthPlayer, int heightPlayer, int xPlayer, int yPlayer, Bundle userData) {
         if (containerView == null) return;
         Bundle params = getIntent().getExtras();
         String interactiveLink = ""; // or other values
-        if(params != null){
+        if (params != null) {
             interactiveLink = params.getString("interactiveLink");
         }
         String url = interactiveLink.length() > 0 ? interactiveLink : HTML_SDK.replace("[SDK_VERSION]", VERSION);
-//        SigmaWebViewCallback sigmaWebviewCallback = new SigmaWebViewCallback() {
-//            @Override
-//            public void onReady() {
-//                Log.d("sigmaWebviewCallback=>", "ready");
-//            }
-//
-//            @Override
-//            public void onKeyDown(int code) {
-//                Log.d("sigmaWebviewCallback=>", "onKeyDown");
-//            }
-//
-//            @Override
-//            public void onOverlayShow() {
-//                Log.d("sigmaWebviewCallback=>", "onOverlayShow");
-//            }
-//
-//            @Override
-//            public void onOverlayHide() {
-//                Log.d("sigmaWebviewCallback=>", "onOverlayHide");
-//            }
-//
-//            @Override
-//            public void onForceFullScreen() {
-//                Log.d("sigmaWebviewCallback=>", "onForceFullScreen");
-//            }
-//
-//            @Override
-//            public void onExitFullScreen() {
-//                Log.d("sigmaWebviewCallback=>", "onExitFullScreen");
-//            }
-//        };
-        SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).openInteractiveView(x, y, width, height, widthPlayer, heightPlayer, xPlayer, yPlayer, null, url, null);
+        SigmaWebViewCallback sigmaWebviewCallback = new SigmaWebViewCallback() {
+            //Sự kiện khi sdk tương tác sẵn sàng
+            @Override
+            public void onReady() {
+                JSONObject userDataSend = new JSONObject();
+                if (userData != null) {
+                    Set<String> keys = userData.keySet();
+                    for (String key : keys) {
+                        try {
+                            userDataSend.put(key, JSONObject.wrap(userData.get(key)));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                SigmaWebView interactiveView = SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).getInteractiveView();
+                Log.d("onReady=>", userDataSend.toString());
+                if (interactiveView != null) {
+                    if (xInteractiveView != 0 || yInteractiveView != 0 || widthInteractiveView != FrameLayout.LayoutParams.MATCH_PARENT || heightInteractiveView != FrameLayout.LayoutParams.MATCH_PARENT) {
+                        SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).setLayoutInteractiveView(xInteractiveView, yInteractiveView, widthInteractiveView, heightInteractiveView, widthPlayer, heightPlayer, xPlayer, yPlayer);
+                    }
+                    interactiveView.sendOnReadyBack(userData != null ? userDataSend.toString() : "{}");
+                    double leftP = (double) xPlayer / widthInteractiveView, rightP = (double) (widthPlayer + xPlayer) / widthInteractiveView, topP = (double) yPlayer / heightInteractiveView, bottomP = (double) (yPlayer + heightPlayer) / heightInteractiveView;
+                    interactiveView.sendRectPlayer(leftP, rightP, bottomP, topP);
+                    interactiveView.requestFocus();
+                }
+            }
+
+            @Override
+            public void onKeyDown(int code) {
+
+            }
+
+            //Sự kiện khi overlay hiển thị
+            @Override
+            public void onOverlayShow() {
+                Log.d("PlayerActivity=>", "onOverlayShow");
+            }
+            //Sự kiện khi overlay tắt
+            @Override
+            public void onOverlayHide() {
+                Log.d("PlayerActivity=>", "onOverlayHide");
+            }
+
+            //Sự kiện khi hệ thống tương tác yêu cầu player full màn hình
+            @Override
+            public void onForceFullScreen() {
+                Log.d("PlayerActivity=>", "onForceFullScreen");
+                PlayerActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
+
+            //Sự kiện khi hệ thống tương tác yêu cầu player thoát full màn hình
+            @Override
+            public void onExitFullScreen() {
+                Log.d("PlayerActivity=>", "onExitFullScreen");
+                PlayerActivity.this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            }
+        };
+        SigmaInteractiveHelper.getInstance(containerView, PlayerActivity.this).openInteractiveViewWithCallback(widthInteractiveView, heightInteractiveView, url, sigmaWebviewCallback);
     }
 
     @Override
